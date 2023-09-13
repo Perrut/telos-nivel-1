5d139dacfb1994e76fcf234b822aed9eb1243fe6
Matheus Perrut <matheus.perrut@govbr.com.br>
2023-09-13 18:36:50 -0300

    array review

---
+++ 04-arrays/04-arrays.js
@@ -0,0 +1,22 @@
+// inicializando um array
+var array = [];
+
+// inicializando um array com valores
+var arrayWithValues = [1,2,3,4,5,6];
+
+// propriedade importante do objeto array -> length
+console.log(array.length);
+console.log(arrayWithValues.length);
+
+// acessando um elemento do array
+// sempre se lembrar que o primeiro índice de um array é 0
+// e o último é o tamanho do array - 1
+// primeiro elemento do array
+console.log(arrayWithValues[0]);
+// ultimo elemento do array
+console.log(arrayWithValues[5]);
+// quando queremos acessar um elemento que está em uma posição ordinal no array
+// basta diminuir 1 do valor dessa posição ordinal e encontraremos o elemento
+// terceiro elemento do array
+// 3 - 1 = 2
+console.log(arrayWithValues[2]);
