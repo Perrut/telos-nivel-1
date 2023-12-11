9dc429670d637a508bce89b2a9eb47a41586959d
Matheus Perrut <matheus.perrut@govbr.com.br>
2023-12-11 20:59:11 -0300

    setDate examples

--- 07-date/07-date.js
+++ 07-date/07-date.js
@@ -17,7 +17,7 @@
 // console.log(new Date(2023, 8, 13, 20, 30, 15));
 // console.log(new Date(2023, 8, 13, 20, 30, 15, 30));
 
-var date = new Date();
+// var date = new Date();
 // lembrar que os métodos que obtém propriedades de data sem UTC
 // utilizam o fuso horário do sistema operacional
 // console.log(date.getDate(), date.getUTCDate());
@@ -27,6 +27,47 @@ var date = new Date();
 // console.log(date.getMinutes(), date.getUTCMinutes());
 // console.log(date.getSeconds(), date.getUTCSeconds());
 
-console.log(date.getTime()); // timestamp
+// console.log(date.getTime()); // timestamp
 // timestamp é o valor em milissegundos que se passaram entre a data e o primeiro dia
 // de 1970
+
+// configurando campos de data
+var date = new Date();
+console.log('Data original', date);
+
+date.setUTCDate(1);
+console.log('setUTCDate', date);
+date.setUTCMonth(1);
+console.log('setUTCMonth', date);
+date.setUTCFullYear(1994);
+console.log('setUTCFullYear', date);
+
+date.setUTCHours(10);
+console.log('setUTCHours', date);
+date.setUTCMinutes(10);
+console.log('setUTCMinutes', date);
+date.setUTCSeconds(10);
+console.log('setUTCSeconds', date);
+
+console.log('');
+
+date.setDate(2);
+console.log('setDate', date);
+date.setMonth(4);
+console.log('setMonth', date);
+date.setFullYear(2005);
+console.log('setFullYear', date);
+
+date.setHours(11);
+console.log('setHours', date);
+date.setMinutes(11);
+console.log('setMinutes', date);
+date.setSeconds(11);
+console.log('setSeconds', date);
+
+console.log('');
+date.setUTCMilliseconds(0);
+console.log('setUTCMilliseconds', date);
+
+date.setMilliseconds(10);
+console.log('setMilliseconds', date);
