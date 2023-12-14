38755c383978e53d7a0eb278a4a97af21040892c
Matheus Perrut <matheus.perrut@govbr.com.br>
2023-12-13 22:03:52 -0300

    objects exercise 4

---
+++ 02-exercicios/02-objetos/04-objetos.js
@@ -0,0 +1,13 @@
+// Exercício 04
+// Gerar um Número Aleatório:
+// Escreva uma função que utilize o método Math.random() para gerar e retornar um
+// número decimal aleatório entre 0 (inclusive) e 1 (exclusive).
+// 
+// Documentação Math.random()
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+function gerarNumeroAleatorio() {
+    return Math.random();
+}
+
+console.log(gerarNumeroAleatorio());
